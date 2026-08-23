2b485337bf33b28b7eb3eb2b1311282f22eb27e8
Rrycbarm <federalex1998@gmail.com>
2026-08-23 20:27:26 +0200

    Fix: markdown was committed as an embedded git repository

--- download_data.sh
+++ download_data.sh
@@ -7,4 +7,6 @@ git clone --depth 1 --filter=blob:none --sparse \
 git -C "$DEST" sparse-checkout set "dnd/521/markdown"
 mv $DEST/dnd/521/markdown/* $DEST
 rm -rf $DEST/dnd
+rm -rf "$DEST/.git"
+touch "$DEST/.gitkeep"
 echo "OK: $(find "$DEST" -name '*.md' | wc -l) file"
